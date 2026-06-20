b68d75c8e14c4200ac0b9b1a5714368ed4f4d845
Raman Maksimchuk <dotnet044@gmail.com>
2026-06-20 13:26:54 +0300

    Add Acceptance testing project

---
+++ acceptance/IFluentStepBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Ocelot.Discovery.KubeClient.Acceptance;
+
+public static class IFluentStepBuilderExtensions // TODO Reuse from Ocelot.Testing
+{
+    public static IFluentStepBuilder<TScenario> AndIf<TScenario>(this IFluentStepBuilder<TScenario> scenario, bool when, Expression<Action<TScenario>> step)
+        where TScenario : class
+        => when ? scenario.And(step) : scenario;
+    public static IFluentStepBuilder<TScenario> AndIfNot<TScenario>(this IFluentStepBuilder<TScenario> scenario, bool when, Expression<Action<TScenario>> step)
+        where TScenario : class
+        => !when ? scenario.And(step) : scenario;
+
+    public static IFluentStepBuilder<TScenario> ThenIf<TScenario>(this IFluentStepBuilder<TScenario> scenario, bool when, Expression<Action<TScenario>> step)
+        where TScenario : class
+        => when ? scenario.Then(step) : scenario;
+    public static IFluentStepBuilder<TScenario> ThenIfNot<TScenario>(this IFluentStepBuilder<TScenario> scenario, bool when, Expression<Action<TScenario>> step)
+        where TScenario : class
+        => !when ? scenario.Then(step) : scenario;
+}
---
+++ acceptance/Steps.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using _KubeClient_ = KubeClient;
+
+namespace Ocelot.Discovery.KubeClient.Acceptance;
+
+public class Steps : AcceptanceSteps
+{
+    public static JsonSerializerSettings JsonSerializerSettings
+        => _KubeClient_.ResourceClients.KubeResourceClient.SerializerSettings;
+
+}
